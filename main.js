a62aad5e0d5d3c4c10c2168b5ee24cfcade2e7a2
MuhammadArbiIIT <marbi@hawk.iit.edu>
2023-11-27 22:29:55 -0600

    fixed id issue with button geo location

--- main.js
+++ main.js
@@ -1,28 +1,32 @@
-document.addEventListener('DOMContentLoaded', getLocationOnLoad);
-document.getElementById('getLocationButton').addEventListener('click', getLocation);
+    document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('getLocationButton').addEventListener('click', getLocation);
+});
 
 function getLocation() {
+    console.log("Button clicked");
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition, showError); 
+        navigator.geolocation.getCurrentPosition(showPosition, showError);
     } else {
         document.getElementById('locationResult').innerHTML = "Geolocation is not supported by this browser.";
     }
 }
 
-function showPosition (position) {
-    const latitude = position.coords.lattitude;
-    const longitude = positon.coords.longitude;
-    const accuracy = positon.coords.accuracy;
+function showPosition(position) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    const accuracy = position.coords.accuracy;
 
-    document.getElementById('locationResult').innerHTML = 
-    `Latitude: ${latitude}<br>Longitude: ${longitude}<br>Accuracy: ${accuracy} meters`;
-
+    document.getElementById('locationResult').innerHTML =
+        `Latitude: ${latitude}<br>Longitude: ${longitude}<br>Accuracy: ${accuracy} meters`;
 }
 
-function showError (error) {
+function showError(error) {
     switch (error.code) {
         case error.PERMISSION_DENIED:
-        document.getElementById('locationResult').innerHTML = "User denied the request for Geolocation.";
-        break;
+            document.getElementById('locationResult').innerHTML = "User denied the request for Geolocation.";
+            break;
+        default:
+            document.getElementById('locationResult').innerHTML = "An unspecified error occurred.";
+            break;
     }
 }
