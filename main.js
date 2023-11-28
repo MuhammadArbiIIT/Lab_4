f74ef3f367417aad62a2acb4d322c1afbaf51e55
MuhammadArbiIIT <marbi@hawk.iit.edu>
2023-11-28 01:27:26 -0600

    function to gather specific cities from the sunrise sunset database

--- main.js
+++ main.js
@@ -1,7 +1,33 @@
-    document.addEventListener('DOMContentLoaded', function() {
+//function for button when clicked to get the location
+document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('getLocationButton').addEventListener('click', getLocation);
+
+//Fetch selected cities
+const cities = ['New York', 'Chicago','Los Angeles','Boston','Toronto'];
+fetchCityData(cities);
 });
 
+function fetchCityData(cities) {
+    const apiUrl = 'https://sunrisesunset.io/api/v1/sun?';
+    
+    //Create an array of promises for each city
+    const promises = cities.map(city => {
+        const cityUrl = `${apiUrl}&city=${encodeURIComponent(city)}`;
+        return fetch(cityUrl)
+        .then(response => response.json())
+        .then(data => {
+            return {
+                city, 
+                sunrise: data.results.sunrise,
+                sunset: data.results.senset
+
+            };
+        });
+    });
+
+}
+
+//used for button clicked and show error
 function getLocation() {
     console.log("Button clicked");
     if (navigator.geolocation) {
@@ -10,7 +36,7 @@ function getLocation() {
         document.getElementById('locationResult').innerHTML = "Geolocation is not supported by this browser.";
     }
 }
-
+//getting coords and getting information frrom api
 function showPosition(position) {
     const latitude = position.coords.latitude;
     const longitude = position.coords.longitude;
@@ -19,7 +45,7 @@ function showPosition(position) {
     document.getElementById('locationResult').innerHTML =
         `Latitude: ${latitude}<br>Longitude: ${longitude}<br>Accuracy: ${accuracy} meters`;
 }
-
+//error handeling
 function showError(error) {
     switch (error.code) {
         case error.PERMISSION_DENIED:
