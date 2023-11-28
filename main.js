40c16a5a22cf69c2230ad36516cda47a06f72939
MuhammadArbiIIT <marbi@hawk.iit.edu>
2023-11-28 02:08:28 -0600

    populates the dropdown menu to a separate function and ensure it's called only once when the page loads

--- main.js
+++ main.js
@@ -1,70 +1,88 @@
 document.addEventListener('DOMContentLoaded', function() {
+    const cityDropdown = document.getElementById('cityDropdown');
+    const locationResult = document.getElementById('locationResult');
+    let cityData = [];
+
+    // Add event listener for city selection
+    cityDropdown.addEventListener('change', function() {
+        const selectedCity = cityDropdown.value;
+        if (selectedCity) {
+            displaySelectedCity(selectedCity);
+        }
+    });
+
+    // Add event listener for button click
     document.getElementById('getLocationButton').addEventListener('click', getLocation);
 
     // Fetch selected cities
     const cities = ['New York', 'Chicago', 'Los Angeles', 'Boston', 'Toronto'];
+
+    // Fetch initial city data and populate dropdown
     fetchCityData(cities);
+    populateDropdown(cities);
+
+    // Function to fetch city data
+    function fetchCityData(cities) {
+        const apiUrl = 'https://api.sunrise-sunset.org/json?';
+
+        // Create an array of promises for each city
+        const promises = cities.map(city => {
+            const cityUrl = `${apiUrl}&q=${encodeURIComponent(city)}`;
+            return fetch(cityUrl)
+                .then(response => response.json())
+                .then(data => ({
+                    city,
+                    sunrise: data.results.sunrise,
+                    sunset: data.results.sunset
+                }))
+                .catch(error => {
+                    console.error(`Error fetching data for ${city}:`, error);
+                    return {
+                        city,
+                        sunrise: 'N/A',
+                        sunset: 'N/A'
+                    };
+                });
+        });
+
+        // Resolve all promises
+        Promise.all(promises)
+            .then(newCityData => {
+                // Update the cityData array
+                cityData = newCityData;
+            })
+            .catch(error => {
+                console.error('Error fetching city data:', error);
+            });
+    }
+
+    // Function to populate dropdown menu
+    function populateDropdown(cities) {
+        cities.forEach(city => {
+            const option = document.createElement('option');
+            option.value = city;
+            option.textContent = city;
+            cityDropdown.appendChild(option);
+        });
+    }
+
+    // Function to display information for the selected city
+    function displaySelectedCity(selectedCity) {
+        // Find the data for the selected city in the cityData array
+        const selectedCityData = cityData.find(city => city.city === selectedCity);
+
+        // Display the information for the selected city
+        if (selectedCityData) {
+            const { sunrise, sunset } = selectedCityData;
+            const info = document.createElement('p');
+            info.innerHTML = `<strong>${selectedCity}:</strong> Sunrise at ${sunrise}, Sunset at ${sunset}`;
+            locationResult.innerHTML = '';
+            locationResult.appendChild(info);
+        }
+    }
 });
 
-function fetchCityData(cities) {
-    const apiUrl = 'https://api.sunrise-sunset.org/json?';
-
-    // Create an array of promises for each city
-    const promises = cities.map(city => {
-        const cityUrl = `${apiUrl}&q=${encodeURIComponent(city)}`;
-        return fetch(cityUrl)
-            .then(response => response.json())
-            .then(data => ({
-                city,
-                sunrise: data.results.sunrise,
-                sunset: data.results.sunset
-            }))
-            .catch(error => {
-                console.error(`Error fetching data for ${city}:`, error);
-                return {
-                    city,
-                    sunrise: 'N/A',
-                    sunset: 'N/A'
-                };
-            });
-    });
-
-    // Resolve all promises
-    Promise.all(promises)
-        .then(cityData => {
-            // Display the sunrise and sunset information
-            displayCityData(cityData);
-        })
-        .catch(error => {
-            console.error('Error fetching city data:', error);
-        });
-}
-
-// Function to display city data
-function displayCityData(cityData) {
-    const cityDropdown = document.getElementById('cityDropdown');
-    const locationResult = document.getElementById('locationResult');
-
-    // Clear previous data
-    locationResult.innerHTML = '';
-    cityDropdown.innerHTML = ''; // Clear the dropdown before populating
-
-    // Display sunrise and sunset information for each city
-    cityData.forEach(data => {
-        const { city, sunrise, sunset } = data;
-        const info = document.createElement('p');
-        info.innerHTML = `<strong>${city}:</strong> Sunrise at ${sunrise}, Sunset at ${sunset}`;
-        locationResult.appendChild(info);
-
-        // Populate the dropdown after displaying city data
-        const option = document.createElement('option');
-        option.value = city;
-        option.textContent = city;
-        cityDropdown.appendChild(option);
-    });
-}
-
-// Used for button clicked and show error
+// Function for button click and show error
 function getLocation() {
     console.log("Button clicked");
     if (navigator.geolocation) {
@@ -74,7 +92,7 @@ function getLocation() {
     }
 }
 
-// Getting coords and getting information from API
+// Function to get coordinates and information from API
 function showPosition(position) {
     const latitude = position.coords.latitude;
     const longitude = position.coords.longitude;
@@ -84,7 +102,7 @@ function showPosition(position) {
         `Latitude: ${latitude}<br>Longitude: ${longitude}<br>Accuracy: ${accuracy} meters`;
 }
 
-// Error handling
+// Function for error handling
 function showError(error) {
     switch (error.code) {
         case error.PERMISSION_DENIED:
