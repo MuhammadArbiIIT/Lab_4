41730689bcb1d60f0b3ee0d7d6e6fbdb83a1445d
MuhammadArbiIIT <marbi@hawk.iit.edu>
2023-11-28 01:44:23 -0600

    fix the script to populate the dropdown menu when clicked

--- main.js
+++ main.js
@@ -1,61 +1,70 @@
-//function for button when clicked to get the location
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('getLocationButton').addEventListener('click', getLocation);
 
-//Fetch selected cities
-const cities = ['New York', 'Chicago','Los Angeles','Boston','Toronto'];
-fetchCityData(cities);
+    // Fetch selected cities
+    const cities = ['New York', 'Chicago', 'Los Angeles', 'Boston', 'Toronto'];
+    fetchCityData(cities);
 });
 
 function fetchCityData(cities) {
-    const apiUrl = 'https://sunrisesunset.io/api/v1/sun?';
-    
-    //Create an array of promises for each city
-    const promises = cities.map(city => {
-        const cityUrl = `${apiUrl}&city=${encodeURIComponent(city)}`;
-        return fetch(cityUrl)
-        .then(response => response.json())
-        .then(data => {
-            return {
-                city, 
-                sunrise: data.results.sunrise,
-                sunset: data.results.senset
+    const apiUrl = 'https://api.sunrise-sunset.org/json?';
 
-            };
+    // Create an array of promises for each city
+    const promises = cities.map(city => {
+        const cityUrl = `${apiUrl}&q=${encodeURIComponent(city)}`;
+        return fetch(cityUrl)
+            .then(response => response.json())
+            .then(data => ({
+                city,
+                sunrise: data.results.sunrise,
+                sunset: data.results.sunset
+            }))
+            .catch(error => {
+                console.error(`Error fetching data for ${city}:`, error);
+                return {
+                    city,
+                    sunrise: 'N/A',
+                    sunset: 'N/A'
+                };
+            });
+    });
+
+    // Resolve all promises
+    Promise.all(promises)
+        .then(cityData => {
+            // Display the sunrise and sunset information
+            displayCityData(cityData);
+        })
+        .catch(error => {
+            console.error('Error fetching city data:', error);
         });
-    });
 }
-//resolve all promises
-Promise.all(promises)
-    .then(cityData => {
-        //display the sunrise and sunset information
-        diplayCityData(cityData);
-    })
-    .catch(error => {
-        consolse.error('Error fetching the data', error);
-    });
-//function display city data
+
+// Function to display city data
 function displayCityData(cityData) {
     const cityDropdown = document.getElementById('cityDropdown');
     const locationResult = document.getElementById('locationResult');
-    //clear previous data
-    locationResult.innerHTML= '';
 
-    //diplay sunrise and sunset information for each city
+    // Clear previous data
+    locationResult.innerHTML = '';
+    cityDropdown.innerHTML = ''; // Clear the dropdown before populating
+
+    // Display sunrise and sunset information for each city
     cityData.forEach(data => {
-        const {city, sunrise, sunset } = data;
+        const { city, sunrise, sunset } = data;
         const info = document.createElement('p');
         info.innerHTML = `<strong>${city}:</strong> Sunrise at ${sunrise}, Sunset at ${sunset}`;
         locationResult.appendChild(info);
-        //add each city to the dropdown menu
+
+        // Populate the dropdown after displaying city data
         const option = document.createElement('option');
-        option.value=city;
+        option.value = city;
         option.textContent = city;
         cityDropdown.appendChild(option);
     });
 }
 
-//used for button clicked and show error
+// Used for button clicked and show error
 function getLocation() {
     console.log("Button clicked");
     if (navigator.geolocation) {
@@ -64,7 +73,8 @@ function getLocation() {
         document.getElementById('locationResult').innerHTML = "Geolocation is not supported by this browser.";
     }
 }
-//getting coords and getting information frrom api
+
+// Getting coords and getting information from API
 function showPosition(position) {
     const latitude = position.coords.latitude;
     const longitude = position.coords.longitude;
@@ -73,7 +83,8 @@ function showPosition(position) {
     document.getElementById('locationResult').innerHTML =
         `Latitude: ${latitude}<br>Longitude: ${longitude}<br>Accuracy: ${accuracy} meters`;
 }
-//error handeling
+
+// Error handling
 function showError(error) {
     switch (error.code) {
         case error.PERMISSION_DENIED:
